7277646c188f6a46486b3aab560c32ab59e4b976
Håkan Nilsson <haakan@gmail.com>
2015-02-25 16:52:51 +0100

    Add some doc

--- src/clippy.erl
+++ src/clippy.erl
@@ -1,6 +1,19 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% @doc
 %%%
+%%% Small interface to xclip from Erlang.
+%%%
+%%% Example usage:
+%%%
+%%%   1> clippy:get().
+%%%   "Content of clipboard"
+%%%
+%%%   2> clippy:put(hello).
+%%%   ok
+%%%
+%%%   3> clippy:get().
+%%%   hello
+%%%
 %%% @copyright 2014 Håkan Nilsson
 %%% @end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
